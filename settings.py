61df86e4720b7a31cfe52bcefabad4f5695dbb5a
Simon Lemieux <lemieux.simon@gmail.com>
2015-08-09 18:21:56 -0400

    reading the config using the environment variables

--- settings.py
+++ settings.py
@@ -1,3 +1,9 @@
-import json
+import os, json
 
-config = json.load(open('config.json'))
+# read some infos for our config
+data_dir = os.environ.get('OPENSHIFT_DATA_DIR', '.')
+config = json.load(open(os.path.join(data_dir, 'config.json')))
+
+# and a bit of a hack to add the data_dir to the image_dir folders
+for blob in config['installations']:
+    blob['image_dir'] = os.path.join(data_dir, blob['image_dir'])
