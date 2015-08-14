7cc63c95fa54926d26efa67047c482c3c46b7449
Simon Lemieux <lemieux.simon@gmail.com>
2015-08-13 23:54:43 -0400

    add support for extra_template dir

--- settings.py
+++ settings.py
@@ -7,3 +7,8 @@ config = json.load(open(os.path.join(data_dir, 'config.json')))
 # and a bit of a hack to add the data_dir to the image_dir folders
 for blob in config['installations']:
     blob['image_dir'] = os.path.join(data_dir, blob['image_dir'])
+
+# same for extra templates
+if 'extra_templates' in config:
+    config['extra_templates'] = [os.path.join(data_dir, 'extra_templates', x)
+                                 for x in config['extra_templates']]
